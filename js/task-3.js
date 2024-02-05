d718475d2452263d429b196951ad120d9c668161
d-sofiika <soniadrozd2709@gmail.com>
2024-02-05 16:18:39 +0100

    done 4 tasks!

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,12 @@
-function getElementWidth(content, padding, border){
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
+function checkForSpam(message) {
+    const lowerMessage = message.toLowerCase();
+    return lowerMessage.includes('spam') || lowerMessage.includes('sale');
+}
 
-return content + padding * 2 + border * 2
-};
-
-console.log (getElementWidth("50px", "8px", "4px"));
-console.log (getElementWidth("60px", "12px", "8.5px"));
-console.log (getElementWidth("200px", "0px", "0px"));
-
-
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
